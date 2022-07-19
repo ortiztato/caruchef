fdbaaf3528ec6cea39711ff093b028fb3178f75e
unknown <ortiztato@gmail.com>
2022-07-19 17:22:00 -0600

    avanzado la galeria y mas detalles

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 const contactbtn = document.querySelector("#contactbtn");
-const serviciosbtn = document.querySelector("#serviciosbtn");
+const galeriabtn = document.querySelector("#galeriabtn");
 const cateringbtn = document.querySelector("#cateringbtn");
 
 
@@ -13,6 +13,6 @@ function toggleMenu() {
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 contactbtn.addEventListener("click", toggleMenu);
-serviciosbtn.addEventListener("click", toggleMenu);
+galeriabtn.addEventListener("click", toggleMenu);
 cateringbtn.addEventListener("click", toggleMenu);
 
