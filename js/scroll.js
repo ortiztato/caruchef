1ed54720696ab1b375a8703875c15d33c9f409f7
unknown <ortiztato@gmail.com>
2022-10-04 14:39:05 -0300

    mod js scroll e imagenes

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,9 @@
 
 
-window.onscroll = () => {
+/* window.onscroll = () => {
 
     if (window.scrollY >= window.innerHeight + 2500) {
         document.querySelector(".galeria").style.cssText = "display: block";
     }
 
-}
+} */
